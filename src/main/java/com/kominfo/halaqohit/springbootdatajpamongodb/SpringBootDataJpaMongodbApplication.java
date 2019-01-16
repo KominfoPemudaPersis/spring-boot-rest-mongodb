b0761a3eb189ff8dddbe6e80df501672a144df5d
Hendi Santika <hendisantika@gmail.com>
2019-01-17 05:47:05 +0700

    #1 - Add initial Commit

---
+++ src/main/java/com/kominfo/halaqohit/springbootdatajpamongodb/SpringBootDataJpaMongodbApplication.java
@@ -0,0 +1,14 @@
+package com.kominfo.halaqohit.springbootdatajpamongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataJpaMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDataJpaMongodbApplication.class, args);
+    }
+
+}
+
